1afc3ee6b686713bff7cb934ddf15bd1bef9897f
koudai <moreradio0224@gmail.com>
2018-11-02 23:59:23 +0000

    statusを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181031172011) do
+ActiveRecord::Schema.define(version: 20181102125251) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
